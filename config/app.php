7fc9c96ccc27c57bee54537d81570aeb6a454414
Francisco Javier Moreno García <elboqueronpaco@gmail.com>
2019-01-25 17:13:59 +0100

    corrección app

--- config/app.php
+++ config/app.php
@@ -5,6 +5,6 @@ define('APP_PATH',  __DIR__ . '/..');
 #Autoload Composer
 require_once APP_PATH . '/vendor/autoload.php';
 #Variable de entorno
-require_once './env.php';
+require_once 'env.php';
 #database
-require_once './database.php';
+require_once 'database.php';
